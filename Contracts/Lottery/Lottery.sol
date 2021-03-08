a682b372b8f769ab9a8e164128a71548f820693c
Aram Bayadyan <aram.bv@gmail.com>
2021-03-09 02:46:49 +0400

    lottery contract added

---
+++ Contracts/Lottery/Lottery.sol
@@ -0,0 +1,40 @@
+pragma solidity 0.7.4;
+
+contract Lottery {
+    address public manager;
+    address payable[] public players;
+    
+    constructor() {
+        manager = msg.sender;
+    }
+    
+    function enter() public onlyPlayer("Manager can't enter the lottery") payable {
+        require(msg.value > 0.01 ether, "Ether vlaue should be bigger than 0.01 to enter the lottery");
+        players.push(msg.sender);
+    }
+    
+    function notReallyRandom() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+    
+    function getPrizePool() public view onlyManager("Restricted to manager only") returns(uint) {
+        return address(this).balance;
+    }
+    
+    function pickWinner() public onlyManager("Winner can be picked only by manager") returns (address) {
+        address payable winner = players[notReallyRandom()%players.length];
+        winner.transfer(getPrizePool());
+        players = new address payable[](0);
+        return winner;
+    }
+    
+    modifier onlyManager(string memory message) {
+      require(msg.sender == manager, message);
+      _;
+    }
+    
+    modifier onlyPlayer(string memory message) {
+      require(msg.sender != manager, message);
+      _;
+    }
+}
